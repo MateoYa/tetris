bfe609811dacfd29e177f525919582a8afc4077a
BuildTools <unconfigured@null.spigotmc.org>
2020-11-06 00:09:10 -0800

    the board is proper hight and width. all the pixels get randomized at the bigening everything works correctly

--- Main.py
+++ Main.py
@@ -4,26 +4,27 @@ import numpy as np
 from random import randint
 pixels = []
 colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 255)]
-for y in range(10):
+for y in range(20):
     pixels.append([randint(0, len(colors)) for x in range(10)])
 for pixel in pixels:
     print(pixel)
 
 pixels[0][0] = 5
-SIZE = 10
+SIZE = (20, 10, 3)
 
 while True:
-    env = np.zeros((SIZE, SIZE, 3), dtype=np.uint8)
-    row = 0
-    column = 0
+    env = np.zeros(SIZE, dtype=np.uint8)
+    row = -1
+    column = -1
     for pixel_row in pixels:
         column += 1
-        row = 0
+        row = -1
+        print("hi")
         for pixel in pixel_row:
             row += 1
-            env[column-1][row-1] = colors[pixel-1]
+            env[column][row] = colors[pixel-1]
     img = Image.fromarray(env, "RGB")
-    img = img.resize((300, 300), resample=Image.NEAREST)
+    img = img.resize((300, 600), resample=Image.NEAREST)
     img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
     cv2.imshow("", img)
     cv2.waitKey(0)
