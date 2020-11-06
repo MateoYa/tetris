bf2c76b0df895b6cd3990bf3d11ef9de2b89c4d5
BuildTools <unconfigured@null.spigotmc.org>
2020-11-06 12:56:05 -0800

    the game is emtpy exsept 1 tile top left corner

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ import cv2
 import numpy as np
 from random import randint
 pixels = []
-colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 255)]
+colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 0, 255), (0, 0, 0)]
 for y in range(20):
-    pixels.append(0 for x in range(10))
+    pixels.append([0 for x in range(10)])
 for pixel in pixels:
     print(pixel)
 
@@ -14,14 +14,14 @@ SIZE = (20, 10, 3)
 
 while True:
     env = np.zeros(SIZE, dtype=np.uint8)
-    row = -1
-    column = -1
+    row = 0
+    column = 0
     for pixel_row in pixels:
         column += 1
-        row = -1
+        row = 0
         for pixel in pixel_row:
             row += 1
-            env[column][row] = colors[pixel-1]
+            env[column-1][row-1] = colors[pixel-1]
     img = Image.fromarray(env, "RGB")
     img = img.resize((300, 600), resample=Image.NEAREST)
     img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
